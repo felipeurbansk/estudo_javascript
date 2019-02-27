e0e72826794eda8eb334862a74fcaf87b457c808
Felipe Urbanski <felipeurbansk@gmail.com>
2019-02-27 08:46:59 -0300

    Aprendendo sobre getter e setter no javascript

--- oritentação_objeto/getter_setter.js
+++ oritentação_objeto/getter_setter.js
@@ -4,6 +4,23 @@ const sequencia = {
     set valor(valor) {
         if(valor > this._valor){
             this._valor = valor
+        }else{
+            console.log('Número informado menor que o já atribuido');
         }   
     }
 }
+
+// Primeira sequencia
+console.log(sequencia.valor, sequencia.valor);
+
+// Adicionando valor 1000, esse valor será passado pelo set 
+sequencia.valor = 1000
+console.log(sequencia.valor, sequencia.valor);
+
+// Tenta adicionar um valor menor e retorna um erro
+sequencia.valor = 800
+console.log(sequencia.valor, sequencia.valor);
+
+// Adiciona um valor menor diretamente na variavel "privada", não fazer isso com variaveis com _
+sequencia._valor = 700
+console.log(sequencia.valor, sequencia.valor);
